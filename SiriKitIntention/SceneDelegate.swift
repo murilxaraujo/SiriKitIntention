74145fbb78cac08a357f5d8da7f5064b0636f6da
Murilo Araujo <muernandes@icloud.com>
2020-04-28 17:10:02 -0300

    Added intent verification in scene delegate

--- SiriKitIntention/SceneDelegate.swift
+++ SiriKitIntention/SceneDelegate.swift
@@ -14,9 +14,19 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        
+        if let _ = userActivity?.interaction?.intent as? DoSomethingIntent {
+
+            if let windowScene = scene as? UIWindowScene {
+                self.window = UIWindow(windowScene: windowScene)
+                let storyboard = UIStoryboard(name: "Main", bundle: nil)
+                let initialViewController = storyboard.instantiateViewController(withIdentifier: "ViewController") as! ViewController
+                self.window!.rootViewController = initialViewController
+                self.window!.makeKeyAndVisible()
+                initialViewController.showMessage()
+            }
+        }
+        
         guard let _ = (scene as? UIWindowScene) else { return }
     }
 
